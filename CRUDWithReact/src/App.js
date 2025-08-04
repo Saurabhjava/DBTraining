1694702e5dd94eb08bf0be120163cd12a5f25046
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2025-08-04 13:15:01 +0530

    Add files via upload

---
+++ CRUDWithReact/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import EmployeeHome from "./components/EmployeeHome";
+function App() {
+  return (
+    <div className="App">
+      <EmployeeHome />
+    </div>
+  );
+}
+
+export default App;
